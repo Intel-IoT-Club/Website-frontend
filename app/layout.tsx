23729d82c1604f2dc1b44a221c1bfc479305fc81
PRIYANSH NARANG <priyanshnarang@PRIYANSHs-MacBook-Air.local>
2025-05-27 18:46:51 +0530

    added emailjs to contact us page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import { ThemeProvider } from "@/components/theme-provider"
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/sonner"
 
 const inter = Inter({ subsets: ["latin"] })
 
